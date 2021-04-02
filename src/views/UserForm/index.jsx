dcafefd752278170426b8216eb15b39994117f8a
vitormedeiros911 <vitormedeiros911@outlook.com>
2021-04-02 12:17:05 -0300

    Adiciona formulário de usuário

--- src/views/UserForm/index.jsx
+++ src/views/UserForm/index.jsx
@@ -1,6 +1,40 @@
-import React from "react";
-import { Text } from "react-native";
+import React, { useState } from "react";
+import { Button, Text, TextInput, View } from "react-native";
 
-export default () => {
-  return <Text>Teste</Text>;
+import styles from "./style";
+
+export default ({ route, navigation }) => {
+  const [user, setUser] = useState(route.params ? route.params : {});
+
+  return (
+    <View style={styles.form}>
+      <Text>Name</Text>
+      <TextInput
+        onChangeText={(name) => setUser({ ...user, name })}
+        placeholder="Informe o Nome"
+        value={user.name}
+        style={styles.input}
+      />
+      <Text>E-mail</Text>
+      <TextInput
+        onChangeText={(email) => setUser({ ...user, email })}
+        placeholder="Informe o E-mail"
+        value={user.email}
+        style={styles.input}
+      />
+      <Text>URL do Avatar</Text>
+      <TextInput
+        onChangeText={(avatarUrl) => setUser({ ...user, avatarUrl })}
+        placeholder="Informe a URL do Avatar"
+        value={user.avatarUrl}
+        style={styles.input}
+      />
+      <Button
+        title="Salvar"
+        onPress={() => {
+          navigation.goBack();
+        }}
+      />
+    </View>
+  );
 };
---
+++ src/views/UserForm/style.js
@@ -0,0 +1,14 @@
+import { StyleSheet } from "react-native";
+
+export default styles =  StyleSheet.create({
+  form: {
+    padding: 12,
+  },
+
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 10
+  }
+})
